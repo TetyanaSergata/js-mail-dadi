9a066c4614f5ff22efa9dcf3959d51163675af82
Tetyana <tsergata@gmail.com>
2020-01-07 16:24:52 +0100

    primo esercizio

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,20 @@
-alert('Ciao');
+// alert('Ciao');
+
+// Esercizio 1
+// Lista Utenti registrati
+var listMail = ['marco@libero.it', 'andrea@yahoo.com', 'lisa@libero.it', 'chiara@gmail.com'];
+// E-mail dell'utente
+var mailUser = prompt('Inserisci la tua e-mail');
+console.log(mailUser);
+
+var permit = false;
+// Ciclo for
+for( var i=0; i<listMail.length; i++){
+  var mail = listMail[i];
+  console.log(mail);
+  if(mailUser==listMail){
+    permit = true;
+    console.log(permit);
+  }
+}
+console.log(permit);
